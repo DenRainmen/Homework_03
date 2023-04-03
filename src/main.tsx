6e29fd6916f8d65d8e917046eed40a20df8ddcb7
Den <potarykindenis@gmail.com>
2023-04-03 15:38:40 +0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+
+import ReactDOM from 'react-dom/client'
+
+import App from './s1-main/App'
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+    
+       
+            <App/>
+        
+   
+)
+
+
+
